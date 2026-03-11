1336ab44b35dcc3c6272fc2f8121a297616b21eb
32555348320 <emreyoruk6464@gmail.com>
2026-03-11 15:46:06 +0300

    feat: response Update

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,14 +1,14 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PackageOutputs, PackageResponse, PackageExecutor, OutputImage
+from components.DemoPackageEmre.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PackageExecutor1Outputs, PackageExecutor1Response, PackageExecutor1, OutputImage
 
 
 def build_response(context):
     outputImage = OutputImage(value=context.image)
-    Outputs = PackageOutputs(outputImage=outputImage)
-    packageResponse = PackageResponse(outputs=Outputs)
-    packageExecutor = PackageExecutor(value=packageResponse)
-    executor = ConfigExecutor(value=packageExecutor)
+    packageExecutor1Outputs = PackageExecutor1Outputs(outputImage=outputImage)
+    packageExecutor1Response = PackageExecutor1Response(outputs=packageExecutor1Outputs)
+    packageExecutor1 = PackageExecutor1(value=packageExecutor1Response)
+    executor = ConfigExecutor(value=packageExecutor1)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
