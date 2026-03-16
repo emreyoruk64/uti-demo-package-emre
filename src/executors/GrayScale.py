ca8b90de856b1c964de03b82a8b05f201ce610e0
32555348320 <emreyoruk6464@gmail.com>
2026-03-16 16:19:31 +0300

    feat: GrayScale Update

--- src/executors/GrayScale.py
+++ src/executors/GrayScale.py
@@ -11,7 +11,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
-from components.DemoPackageEmre.src.utils.response import build_response
+from components.DemoPackageEmre.src.utils.response import build_response_gray
 from components.DemoPackageEmre.src.models.PackageModel import PackageModel
 
 
@@ -37,7 +37,7 @@ class GrayScale(Component):
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
         img.value = self.gray(img.value)
         self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
-        packageModel = build_response(context=self)
+        packageModel = build_response_gray(context=self)
         return  packageModel
 
 
